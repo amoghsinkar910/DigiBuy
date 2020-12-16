2fe0b36a660258de61093d07dfcbb6eef683a6ba
AMOGH <amogh@amogh.com>
2020-12-17 02:20:00 +0530

    addded cards for products

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card,Row} from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({product}) => {
     return (
@@ -13,7 +14,7 @@ const Product = ({product}) => {
                         <Card.Title as='div'> <strong>{product.name}</strong></Card.Title>
                     </a>
                     <Card.Text as='div'>
-                        <div className='my-3'> {product.rating} from {product.numReviews} reviews</div>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
                     </Card.Text>
                     <Card.Text as='h3'>
                         Rs {product.price}
