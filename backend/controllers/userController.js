3d9956ff397a0ee13a52d696bc192a7ebcb714dd
AMOGH <amoghsinkar910@gmail.com>
2021-06-17 01:32:27 +0530

    Add user profile screen, reducer and action

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,38 +24,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    Register users
+// @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body
-  
-    const userExists = await User.findOne({ email })
-    if(userExists){
-        res.status(400)
-        throw new Error("User already exists")
-    }
-    const user = await User.create({
-        name,
-        email,
-        password
-    })
-    if(user)
-    {
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(user._id),
-        })
-    }
-    else
-    {
-        res.status(400)
-        throw new Error("Invalid user data")
-    }
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
   })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
@@ -76,7 +76,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    update user profile
+// @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
@@ -85,11 +85,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name
     user.email = req.body.email || user.email
-    if(req.body.password){
+    if (req.body.password) {
       user.password = req.body.password
     }
 
     const updatedUser = await user.save()
+
     res.json({
       _id: updatedUser._id,
       name: updatedUser.name,
@@ -97,8 +98,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       isAdmin: updatedUser.isAdmin,
       token: generateToken(updatedUser._id),
     })
-  } 
-  else {
+  } else {
     res.status(404)
     throw new Error('User not found')
   }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router,Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
@@ -8,24 +8,25 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main> 
-        <Container className='py-3'> 
-          <Route path='/register' component={RegisterScreen}/> 
-          <Route path='/login' component={LoginScreen}/> 
-          <Route path='/product/:id' component={ProductScreen}/> 
-          <Route path='/cart/:id?' component={CartScreen}/> 
-          <Route path='/' component={HomeScreen} exact/> 
+      <main className='py-3'>
+        <Container>
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
     </Router>
-
-  );
+  )
 }
 
-export default App;
+export default App
